83ad255e03833b49fd33f07e5205ea784b1f1c8d
Jonathan Morales <jonathansme01@gmail.com>
2025-08-14 14:50:06 -0600

    first commit

---
+++ tip_calculator.py
@@ -0,0 +1,19 @@
+# TIP CALCULATOR
+
+print("Welcome to tip calculator".upper())
+print()
+
+bill = float(input('Enter the bill: '))
+tip_percentage = float(input('Enter the tip percentage: '))
+
+if bill < 0 or tip_percentage < 0:
+  print("\nValues must be positive\n")
+
+else:
+  tip = bill * tip_percentage / 100
+  total_bill = bill + tip
+
+  print(f'\nThe tip is: {tip:.2f}') # :.2f ensures the output has exactly two digits after the decimal — ideal for money formatting
+  print(f"The total bill is: {total_bill:.2f}")
+
+  print("\nThank you for using de tip calculator!\n")
